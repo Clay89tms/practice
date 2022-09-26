cee54c566c7193ac8e4ef1926aa4b14c0f9d3c77
Clay89TMS <bboyClay89@gmail.com>
2022-09-26 17:31:10 +0300

    sec commit (+consClass)

---
+++ src/ConsClass.java
@@ -0,0 +1,10 @@
+public class ConsClass {
+    public static void main (String[] args){
+        int sum = 1;
+        for (int i = 1; i<10; i++){
+            sum *= i;
+            System.out.print(" " + sum);
+        }
+        System.out.println("\nsumma = " + sum);
+    }
+}
